ee0a08b2b511b3a1639f7ac7de6527a9d0bc91f4
unknown <housineaf@icould.com>
2026-09-24 22:29:52 +0100

    second

---
+++ test.js
@@ -0,0 +1,58 @@
+const candidats = [
+	{
+		cin: "AB123456",
+		nom: "Boushaba",
+		prenom: "Soufiane",
+		partiPolitique: "Indépendant",
+		age: 40,
+		electeurs: []
+	},
+	{
+		cin: "CD234567",
+		nom: "Alaoui",
+		prenom: "Yassine",
+		partiPolitique: "Parti du Progrès",
+		age: 35,
+		electeurs: []
+	},
+	{
+		cin: "EF345678",
+		nom: "Bennani",
+		prenom: "Amine",
+		partiPolitique: "Indépendant",
+		age: 42,
+		electeurs: []
+	},
+	{
+		cin: "GH456789",
+		nom: "El Mansouri",
+		prenom: "Omar",
+		partiPolitique: "Parti de la Justice",
+		age: 38,
+		electeurs: []
+	},
+	{
+		cin: "IJ567890",
+		nom: "Chakir",
+		prenom: "Hamza",
+		partiPolitique: "Parti du Renouveau",
+		age: 45,
+		electeurs: []
+	},
+	{
+		cin: "KL678901",
+		nom: "Tazi",
+		prenom: "Mehdi",
+		partiPolitique: "Indépendant",
+		age: 33,
+		electeurs: []
+	},
+	{
+		cin: "MN789012",
+		nom: "Amrani",
+		prenom: "Anas",
+		partiPolitique: "Parti Social",
+		age: 41,
+		electeurs: []
+	}
+];
