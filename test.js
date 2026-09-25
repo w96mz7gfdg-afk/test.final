259b73a9a91371671f8d9d444bc7c865c07c7b92
w96mz7gfdg-afk <housineaf@icould.com>
2026-09-25 16:44:36 +0000

    commit5

--- test.js
+++ test.js
@@ -1,103 +1,254 @@
 const prompt = require('prompt-sync')();
-let candidats = [
+const candidats = [
 	{
 		cin: "AB123456",
-		nom: "Boushaba",
-		prenom: "Soufiane",
-		partiPolitique: "Indépendant",
+		nom: "afensou",
+		prenom: "housine",
+		partiPolitique: "waw",
 		age: 40,
-		electeurs: []
+		electeurs: ["CD234567", "GH456789", "MN789012", "GI012456"]
 	},
 	{
 		cin: "CD234567",
 		nom: "Alaoui",
 		prenom: "Yassine",
-		partiPolitique: "Parti du Progrès",
-		age: 35,
+		partiPolitique: "Indépendant",
+		age: 45,
 		electeurs: []
 	},
 	{
 		cin: "EF345678",
 		nom: "Bennani",
-		prenom: "Amine",
-		partiPolitique: "Indépendant",
-		age: 42,
-		electeurs: []
+		prenom: "Salma",
+		partiPolitique: "law",
+		age: 32,
+		electeurs: ["FH901345", "QR901234", "UV123567"]
 	},
 	{
 		cin: "GH456789",
-		nom: "El Mansouri",
-		prenom: "Omar",
-		partiPolitique: "Parti de la Justice",
+		nom: "Chraibi",
+		prenom: "Hamza",
+		partiPolitique: "Indépendant",
 		age: 38,
 		electeurs: []
 	},
 	{
 		cin: "IJ567890",
-		nom: "Chakir",
-		prenom: "Hamza",
-		partiPolitique: "Parti du Renouveau",
-		age: 45,
+		nom: "El Amrani",
+		prenom: "Mehdi",
+		partiPolitique: "jaw",
+		age: 51,
 		electeurs: []
 	},
 	{
 		cin: "KL678901",
-		nom: "Tazi",
-		prenom: "Mehdi",
-		partiPolitique: "Indépendant",
-		age: 33,
-		electeurs: []
+		nom: "Berrada",
+		prenom: "Nadia",
+		partiPolitique: "jaw",
+		age: 43,
+		electeurs: ["ST012345", "YZ345789"]
 	},
 	{
 		cin: "MN789012",
-		nom: "Amrani",
-		prenom: "Anas",
-		partiPolitique: "Parti Social",
+		nom: "Fassi",
+		prenom: "Omar",
+		partiPolitique: "Indépendant",
+		age: 36,
+		electeurs: []
+	},
+	{
+		cin: "OP890123",
+		nom: "Tazi",
+		prenom: "Imane",
+		partiPolitique: "waw",
+		age: 29,
+		electeurs: []
+	},
+	{
+		cin: "QR901234",
+		nom: "Mansouri",
+		prenom: "Karim",
+		partiPolitique: "Indépendant",
+		age: 47,
+		electeurs: []
+	},
+	{
+		cin: "ST012345",
+		nom: "Naciri",
+		prenom: "Aya",
+		partiPolitique: "Indépendant",
+		age: 35,
+		electeurs: []
+	},
+	{
+		cin: "UV123567",
+		nom: "Tahiri",
+		prenom: "Reda",
+		partiPolitique: "Indépendant",
 		age: 41,
+		electeurs: []
+	},
+	{
+		cin: "WX234678",
+		nom: "Zeroual",
+		prenom: "Meryem",
+		partiPolitique: "jaw",
+		age: 39,
+		electeurs: []
+	},
+	{
+		cin: "YZ345789",
+		nom: "Idrissi",
+		prenom: "Ayoub",
+		partiPolitique: "waw",
+		age: 44,
+		electeurs: []
+	},
+	{
+		cin: "AC456890",
+		nom: "Rami",
+		prenom: "Hiba",
+		partiPolitique: "law",
+		age: 31,
+		electeurs: []
+	},
+	{
+		cin: "BD567901",
+		nom: "Kabbaj",
+		prenom: "Anas",
+		partiPolitique: "waw",
+		age: 48,
+		electeurs: ["CE678012"]
+	},
+	{
+		cin: "CE678012",
+		nom: "Mekki",
+		prenom: "Lina",
+		partiPolitique: "Indépendant",
+		age: 28,
+		electeurs: []
+	},
+	{
+		cin: "DF789123",
+		nom: "El Fassi",
+		prenom: "Rachid",
+		partiPolitique: "jaw",
+		age: 53,
+		electeurs: []
+	},
+	{
+		cin: "EG890234",
+		nom: "Ouazzani",
+		prenom: "Sara",
+		partiPolitique: "law",
+		age: 37,
+		electeurs: []
+	},
+	{
+		cin: "FH901345",
+		nom: "Kettani",
+		prenom: "Ilyas",
+		partiPolitique: "Indépendant",
+		age: 34,
+		electeurs: []
+	},
+	{
+		cin: "GI012456",
+		nom: "Mouline",
+		prenom: "Nour",
+		partiPolitique: "Indépendant",
+		age: 42,
 		electeurs: []
 	}
 ];
+function nouveaucandidat() {
+	let obj = {}
+	let kain = false
 
-function nouveaucandidat(){
-	let obj={}
-	let kain=false
-
-     let cin = prompt("entres le cin :");
-	 let nom = prompt("entres le nom :");
-	 let prenom = prompt("entres le prenom :");
-	 let partiPolitique =prompt("entres le artiPolitique :");
-	 let age =+prompt("entres le age :")
-	 for(let i=0 ;i<candidats.length ;i++){ 
-		if(candidats[i].cin===cin){
-			kain=true
+	let cin = prompt("entres le cin :");
+	let nom = prompt("entres le nom :");
+	let prenom = prompt("entres le prenom :");
+	let partiPolitique = prompt("entres le artiPolitique :");
+	let age = +prompt("entres le age :")
+	for (let i = 0; i < candidats.length; i++) {
+		if (candidats[i].cin === cin) {
+			kain = true
 		}
 	}
-	if(kain===false){ 	
-	 obj.cin=cin;
-	 obj.nom=nom;
-	 obj.prenom=prenom;
-	 obj.partiPolitique=partiPolitique;
-	 obj.age=age;
-	 obj.electeurs=[]
-candidats.push(obj);
-console.log(candidats)
-}
-else {
-console.log("maimknch dkhl jouj mrat.");
-
-}
-	 }
-	 nouveaucandidat();
-	 
- function Ajouterplusieurs(){
-	let number =prompt("entres les number des condidat :")
-	for(let i=0 ;i<number;i++){
-	nouveaucandidat();
+	if (kain === false) {
+		obj.cin = cin;
+		obj.nom = nom;
+		obj.prenom = prenom;
+		obj.partiPolitique = partiPolitique;
+		obj.age = age;
+		obj.electeurs = []
+		candidats.push(obj);
+		console.log(candidats)
+	}
+	else {
+		console.log("maimknch dkhl jouj mrat.");
 
 	}
- }
+}
+nouveaucandidat();
+
+function Ajouterplusieurs() {
+	let number = prompt("entres les number des condidat :")
+	for (let i = 0; i < number; i++) {
+		nouveaucandidat();
+
+	}
+}
 Ajouterplusieurs()
 
+function listedescandidats(candidats) {
+	console.log("1-Trier les candidats par nombre de votes");
+	console.log("2-afficher uniquement les candidats d'un parti politique spécifique.");
+	let n = prompt("entre un nombre :")
+	if (n == 1) {
+		let amr;
+		for (let i = 0; i < candidats.length-1; i++) {
+			for (let j = i + 1; j < candidats.length; j++) {
+				if (candidats[i].electeurs.length < candidats[j].electeurs.length) {
+					amr = candidats[i];
+					candidats[i] = candidats[j];
+					candidats[j] = amr
+				}	
+			}
+		}
+		for (let i = 0; i < candidats.length; i++) {
+			console.log("cin :", candidats[i].cin);
+			console.log("nom :", candidats[i].nom);
+			console.log("prenom : ", candidats[i].prenom);
+			console.log("partiPolitique : ", candidats[i].partiPolitique);
+			console.log("age:", candidats[i].age);
+			console.log("electeurs: ", candidats[i].electeurs.length);
+
+		}
+
+
+	}
+
+	else if (n ==2) {
+		console.log("kain waw ou kaina law ou jaw");
+		let partiPolitique=prompt("3tini smya ta3 lhizb li baghi :" )
+		for (let i = 0; i < candidats.length; i++) {
+			if (candidats[i].partiPolitique ===partiPolitique) {
+				console.log("cin :", candidats[i].cin);
+				console.log("nom :", candidats[i].nom);
+				console.log("prenom : ", candidats[i].prenom);
+				console.log("partiPolitique : ", candidats[i].partiPolitique);
+				console.log("age:", candidats[i].age);
+				console.log("electeurs: ", candidats[i].electeurs);
+			}
+		}
+	}
+	else {
+		console.log("choisi 1 ou 2")
+	}
+
+}
+listedescandidats(candidats)
 
 
 
